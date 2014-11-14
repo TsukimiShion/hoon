b01f22368c76e093110126fbfa593f15831216d2
Tsukimi Shion <tsukimishion@gmail.com>
2014-11-14 14:15:49 +0900

    Release the version 1.2 .

---
+++ v1.2/api.js
@@ -0,0 +1,11 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "hoon",
+        "hoon.WebStorage",
+        "hoon.json"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
